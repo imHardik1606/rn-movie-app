51d231c7a08fc4854147e7ebd364fc0f5fe287ef
Hardik Gayner <hardikgayner@gmail.com>
2025-08-05 21:10:50 +0530

    Integrated Appwrite for storing data

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -17,17 +18,26 @@ const Search = () => {
     reset,
   } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
+  // Debounced search effect
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        // storing in appwrite db
+        // Call updateSearchCount only if there are results
+
+        if (movies?.length! > 0 && movies?.[0]) {
+          await updateSearchCount(searchQuery, movies[0]);
+        }
+        
       } else {
         reset();
       }
     }, 500);
 
     return () => clearTimeout(timeoutId);
-  }, [searchQuery]); 
+  }, [searchQuery]);
 
   return (
     <View className="flex-1 bg-primary">
