c08398524f2542f9ba02e7549eeef9a361866208
Hardik Gayner <hardikgayner@gmail.com>
2025-08-07 19:50:49 +0530

    Implemented MoviesDetails Page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,71 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+  const { id } = useLocalSearchParams();
 
-export default MovieDetails
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
+  //function to format votes count
+  const formatNumber = (num: number) => {
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, "") + "k";
+    }
+    return num.toString();
+  };
+
+  return (
+    <View className="bg-primary flex-1">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+      >
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+
+          <View className="flex-row items-center gap-x-1 mt-2">
+            <Text className="text-light-200 text-sm">
+              {movie?.release_date?.split("-")[0]}
+            </Text>
+
+            <Text className="text-light-200 text-sm">{movie?.runtime}m</Text>
+          </View>
+
+          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-4" />
+
+            <Text className="text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+
+            <Text className="text-light-200 text-sm">
+              ({formatNumber(movie?.vote_count || 0)} votes)
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieDetails;
